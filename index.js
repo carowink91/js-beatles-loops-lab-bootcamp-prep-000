1b194c920808a223e7e9c00be42608b42e08d419
angelica winkler <caw400@nyu.edu>
2018-05-10 01:19:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -32,6 +32,7 @@ function johnLennonFacts (facts) {
 function iLoveTheBeatles (n) {
   var newArray = [];
   do {
-    
-  } while {
+    array.push(`I love the Beatles!`);
+    n++;
+  } while (n < 15)
 }
